13afcfe749f5104c96e8efe4fb458f55d6cbb915
unknown <Georges.Samaha@lblGsamaha8.bmb.com.lb>
2014-12-22 00:39:50 +0200

    Created Cache Commands

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,27 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+makeCacheMatrix <- function(x=matrix()) {
+  m <- NULL
+  get <- function() x
+  setImatrix <- function(Imatrix) m <<- Imatrix
+  getImatrix <- function() m
+  
+  list(get=get, setImatrix=setImatrix, getImatrix=getImatrix)
+}
+
+cacheSolve <- function(x) {
+  m <- x$getImatrix()
+  if(!is.null(m)){
+    message("Cached data found. Getting result... Done.")
+    return(m)
+  }
+  else {
+    message("No cached data found. Calculating inverse matrix...")
+    data <- x$get() 
+    m <- solve(data)
+    x$setImatrix(m) 
+    message("Done.")
+    return(m)
+  }
